b67a58c34943b5096cc41664ca4fe41621d0649a
DilshanPathegamage123 <141545307+DilshanPathegamage123@users.noreply.github.com>
2024-01-13 20:15:38 +0530

    Commit 1
    UI of the calculator is created

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { useReducer } from 'react';
 import './App.css';
 
+const ACTIONS ={
+  ADD_DIGIT : 'add-digit',
+  CHOOSE_OPERATION : 'choose-operation',
+  CLEAR : 'clear',
+  DELETE_DIGIT : 'delete-digit',
+  CALCULATE : 'calculate',
+
+}
+
+
 function App() {
+
+  const [{currentOperand, previousoperand, operation}, dispatch] = useReducer(reducer, {})
   return (
-    <div>
-      {/* Add your code here */}
+    <div className='calculator-grid'>
+  <div className='output'>
+    <div className='previous-operand'>{previousoperand} {operation}</div>
+    <div className='current-operand'>{currentOperand}</div>
+  </div>
+  <button className='span-two'>AC</button>
+  <button>DEL</button>
+  <button>÷</button>
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>*</button>
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button>+</button>
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  <button>-</button>
+  <button>.</button>
+  <button>0</button>
+  <button className='span-two'>=</button>
+
     </div>
   );
 }
